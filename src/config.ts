b04c75e440da912d7cde245600e6a0ad9773ae66
lws666 <13318678430@163.com>
2026-04-22 20:20:20 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -445,17 +445,17 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 };
 
 export const commentConfig: CommentConfig = {
-	enable: false, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
-	system: "twikoo", // 评论系统选择: "twikoo" | "giscus"
+	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
+	system: "giscus", // 评论系统选择: "twikoo" | "giscus"
 	twikoo: {
 		envId: "https://twikoo.vercel.app",
 		lang: SITE_LANG,
 	},
 	giscus: {
-		repo: "your-github-username/your-repo-name",
-		repoId: "your-repo-id",
+		repo: "lws666/lws666.github.io",
+		repoId: "R_kgDOSJfTcw",
 		category: "Announcements",
-		categoryId: "your-category-id",
+		categoryId: "DIC_kwDOSJfTc84C7bK6",
 		mapping: "pathname",
 		strict: "0",
 		reactionsEnabled: "1",
